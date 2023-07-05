b1ce622afd72d20d9778f3dd1db7e7374d71fdd8
Fernando Hieda <58814238+Igashe@users.noreply.github.com>
2023-07-05 18:24:11 -0300

    Update Dockerfiles

--- web-backend/src/js/persistence/database.js
+++ web-backend/src/js/persistence/database.js
@@ -3,6 +3,7 @@ import mysql from 'mysql2';
 const pool = mysql.createPool({
     host: "localhost",
     user: "web2",
+    port: "3306",
     password: "1234",
     database: "plshare"
 }).promise();
